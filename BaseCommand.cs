8fb2d12ac185d32b5f1fbbf7d541be419d708c37
Vijayendra Ghatge <vijayendra.ghatge@gmail.com>
2020-02-19 12:36:23 +0000

    Add files via upload

---
+++ BaseCommand.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MarsRovers.ObjectModels
+{
+    public abstract class BaseCommand
+    {
+        protected readonly string _rawCmdText;
+        public string RawCmdText => _rawCmdText;
+
+        protected BaseCommand(string rawTextCmd)
+        {
+            if (string.IsNullOrEmpty(rawTextCmd))
+                throw new ArgumentNullException("Command text cannot be null or empty.");
+
+            _rawCmdText = rawTextCmd;
+        }
+    }
+}
